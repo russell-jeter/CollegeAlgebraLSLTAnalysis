7af8cc9a8411bf3e4dcca6aa2b22e4d2f08567b7
Russell Jeter <rjeter@gsu.edu>
2024-11-18 09:26:16 -0500

    Added PBC to item difficulty.

--- item_difficulty.py
+++ item_difficulty.py
@@ -1,3 +1,4 @@
+import numpy as np
 import utils
 from effective_distractors_analysis import get_distractor_counts_frame
 import matplotlib.pyplot as plt
@@ -41,6 +42,53 @@ def save_item_difficulty_distributions(item_difficulty_frame = None, filename = 
     fig.tight_layout()
     plt.savefig(filename)
 
+def get_student_score_frame(student_responses_with_details = None):
+    if type(student_responses_with_details) == type(None):
+        student_responses_with_details = utils.get_student_responses_with_details()
+
+    student_responses_with_details = utils.get_student_responses_with_details()
+    student_score_frame = student_responses_with_details[["question_id", "student_id", "is_distractor"]].copy()
+    student_score_frame["exam_id"] = student_score_frame["question_id"].str[:2]
+    student_score_frame.loc[:, "question_score"] = (student_score_frame.loc[:, "is_distractor"] == 0).astype(int)
+
+    student_score_frame = student_score_frame.drop(columns=["is_distractor"])
+
+    student_score_frame["exam_score"] = student_score_frame.loc[:, "question_score"]
+
+    student_id_list = np.unique(student_score_frame["student_id"].values)
+
+    for student_id in student_id_list:
+        student_exam_frame = student_score_frame[student_score_frame["student_id"].isin([student_id])]
+        exam_id_list = np.unique(student_exam_frame["exam_id"].values) 
+        for exam_id in exam_id_list:
+            student_score_frame_for_one_exam =  student_exam_frame[student_exam_frame["exam_id"].isin([exam_id])]
+            student_score_frame.loc[student_score_frame_for_one_exam.index.values, "exam_score"] = student_score_frame_for_one_exam["question_score"].sum() - student_score_frame_for_one_exam["question_score"]
+    
+    return student_score_frame
+
+def get_point_biserial_coefficient_frame(student_score_frame = None):
+    if type(student_score_frame) == type(None):
+        student_score_frame = get_student_score_frame()
+
+    point_biserial_correlation_frame = student_score_frame.groupby(by = ["question_id"])[["question_score", "exam_score"]].corr()
+
+    point_biserial_correlation_frame = point_biserial_correlation_frame["question_score"].reset_index()
+    point_biserial_correlation_frame = point_biserial_correlation_frame[point_biserial_correlation_frame["level_1"] == "exam_score"].drop(columns="level_1")
+    point_biserial_correlation_frame = point_biserial_correlation_frame.rename(columns={"question_score": "pbc"})
+    point_biserial_correlation_frame["p_value"] = point_biserial_correlation_frame["pbc"]
+
+    for index, row in point_biserial_correlation_frame.iterrows():
+        question_id = row["question_id"]
+        student_question_response_frame = student_score_frame[student_score_frame["question_id"].isin([question_id])]
+        p_value = student_question_response_frame["question_score"].sum()/len(student_question_response_frame)
+        point_biserial_correlation_frame.loc[index, "p_value"] = p_value
+
+    point_biserial_correlation_frame["exam_id"] = point_biserial_correlation_frame["question_id"].str[0:2]
+
+    return point_biserial_correlation_frame
+
 if __name__ == "__main__":
     print(get_item_difficulty_frame())
     save_item_difficulty_distributions()
+    print(get_student_score_frame())
+    print(get_point_biserial_coefficient_frame())
