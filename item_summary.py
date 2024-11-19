4fa68cb4f6030ff06efc0865ea0da25e593ad6d5
Darryl Chamberlain Jr. <dchamberlain31@gmail.com>
2024-11-19 15:29:39 -0500

    Addition of Rasch analysis py file and added to item_summary

--- item_summary.py
+++ item_summary.py
@@ -2,7 +2,7 @@ import utils
 import pandas as pd
 import effective_distractors_analysis
 import item_difficulty
-
+import rasch_analysis
 
 def get_item_summary_frame(dict_of_dfs = None):
     if type(dict_of_dfs) == type(None):
@@ -10,14 +10,19 @@ def get_item_summary_frame(dict_of_dfs = None):
 
     distractor_counts_frame = effective_distractors_analysis.get_distractor_counts_frame(dict_of_dfs = dict_of_dfs)
     distractor_counts_frame = distractor_counts_frame.rename(columns = {"count": "option_selected_count"})
+
     item_difficulty_frame = item_difficulty.get_item_difficulty_frame().reset_index()
     item_difficulty_frame = item_difficulty_frame.drop(columns = ["exam_id", "index"])
     item_difficulty_frame = item_difficulty_frame.rename(columns = {"count": "student_response_count"})
 
     pbc_frame = item_difficulty.get_point_biserial_coefficient_frame().reset_index()
     pbc_frame = pbc_frame.drop(columns = ["exam_id", "index"])
+
+    rasch_item_frame = rasch_analysis.get_rasch_students_and_items_frames_as_dict()["rasch_items_df"].reset_index()
+
     item_summary_frame = pd.merge(pbc_frame, item_difficulty_frame, on = "question_id")
     #item_summary_frame = pd.merge(distractor_counts_frame, item_summary_frame, on = "question_id")
+    item_summary_frame = pd.merge(rasch_item_frame, item_summary_frame, on = "question_id")
     
     return item_summary_frame
 
