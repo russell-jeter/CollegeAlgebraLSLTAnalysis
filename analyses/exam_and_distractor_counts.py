41bc4ef69a7099769590fe210ac6ca81d7b23b8a
Russell Jeter <rjeter@gsu.edu>
2024-11-20 10:13:45 -0500

    Added docstrings to exam_and_distractor_counts.py

--- analyses/exam_and_distractor_counts.py
+++ analyses/exam_and_distractor_counts.py
@@ -6,6 +6,24 @@ except ImportError:
 import pandas as pd
 
 def show_question_counts(student_responses_with_details = None):
+    """
+    This function prints a summary of the question counts and students' overall performance.
+
+    Parameters
+    ----------
+    student_responses_with_details : Pandas dataframe of student responses to exams with details
+        about the distractors to the question.
+
+        The dataframe has the following fields:
+
+        question_id, student_id, selected_option, option_id, is_distractor, distractor_type, distractor_learning_objective_understanding, notes.
+        
+        Default: None. If no dataframe is given, it will be loaded using database_utils.get_student_responses_with_details()
+
+    Returns
+    -------
+    None. This function prints a summary of the question counts and students' overall performance.
+    """
     if type(student_responses_with_details) == type(None):
         student_responses_with_details = database_utils.get_student_responses_with_details()
     number_of_distractors_chosen = len(student_responses_with_details[student_responses_with_details['is_distractor'] > .5])
@@ -15,6 +33,21 @@ def show_question_counts(student_responses_with_details = None):
     print(f"Number of questions: {number_of_questions}.\nNumber of correct answers chosen: {number_of_correct_answers_chosen}.\nNumber of distractors chosen: {number_of_distractors_chosen}")
 
 def show_exam_question_distractor_counts(dict_of_dfs = None):
+    """
+    This function prints a summary of the distractor selection counts by question_id.
+
+    Parameters
+    ----------
+    dict_of_dfs : Dictionary of dataframes that has the existing frames you want 
+        to manipulated, optional
+        
+        Default: None. If no dictionary is given, the function will load the dictionary
+            using the excel database file in the ./data/ directory.
+
+    Returns
+    -------
+    None. This function prints a summary of the distractor selection counts by question_id.
+    """
     if type(dict_of_dfs) == type(None):
         dict_of_dfs = database_utils.load_database_to_dict_of_dfs()
     
@@ -36,6 +69,21 @@ def show_exam_question_distractor_counts(dict_of_dfs = None):
     print(exam_question_distractor_count_frame)
 
 def show_student_distractor_selection_counts(dict_of_dfs = None):
+    """
+    This function prints a summary of the distractor selection counts by student_id.
+
+    Parameters
+    ----------
+    dict_of_dfs : Dictionary of dataframes that has the existing frames you want 
+        to manipulated, optional
+        
+        Default: None. If no dictionary is given, the function will load the dictionary
+            using the excel database file in the ./data/ directory.
+
+    Returns
+    -------
+    None. This function prints a summary of the distractor selection counts by student_id
+    """
     if type(dict_of_dfs) == type(None):
         dict_of_dfs = database_utils.load_database_to_dict_of_dfs()
     
