00c02a43a22ade9f7bb074e074b91899775d849f
Russell Jeter <rjeter@gsu.edu>
2024-12-26 16:22:03 -0500

    Updated integrated methodology to incorporate acceptable thresholds, too.

--- analyses/integrated_methodology.py
+++ analyses/integrated_methodology.py
@@ -23,12 +23,17 @@ if __name__ == "__main__":
         item_summary_frame = item_summary.get_item_summary_frame()
         item_summary_frame.to_pickle(pickle_filename)
     
+    print(item_summary_frame.columns)
     item_summary_frame = item_summary_frame.sort_values(by = ["question_id"]).reset_index()
     item_summary_frame = item_summary_frame.drop(columns=["index"])
     item_summary_frame["is_good_estimated_item_difficulty"] = 0
     item_summary_frame["is_good_item_difficulty"] = 0
     item_summary_frame["is_good_pbc"] = 0
     item_summary_frame["is_good_effective_distractors"] = 0
+    item_summary_frame["is_acceptable_estimated_item_difficulty"] = 0
+    item_summary_frame["is_acceptable_item_difficulty"] = 0
+    item_summary_frame["is_acceptable_pbc"] = 0
+    item_summary_frame["is_acceptable_effective_distractors"] = 0
     good_estimated_item_indices = item_summary_frame.query("var_estimates_items <= -2 or var_estimates_items >= 2").index
     item_summary_frame.loc[good_estimated_item_indices, "is_good_estimated_item_difficulty"] = 1
     good_item_difficulty_indices = item_summary_frame.query("item_difficulty <= .8 and item_difficulty >= .6").index
@@ -37,6 +42,22 @@ if __name__ == "__main__":
     item_summary_frame.loc[good_pbc_indices, "is_good_pbc"] = 1
     good_effective_distractors_indices = item_summary_frame.query("effective_distractors >= 2").index
     item_summary_frame.loc[good_effective_distractors_indices, "is_good_effective_distractors"] = 1
+
+    acceptable_estimated_item_indices = item_summary_frame.query("var_estimates_items <= -1 or var_estimates_items >= 1").index
+    item_summary_frame.loc[acceptable_estimated_item_indices, "is_acceptable_estimated_item_difficulty"] = 1
+    acceptable_item_difficulty_indices = item_summary_frame.query("item_difficulty <= .9 and item_difficulty >= .5").index
+    item_summary_frame.loc[acceptable_item_difficulty_indices, "is_acceptable_item_difficulty"] = 1
+    acceptable_pbc_indices = item_summary_frame.query("pbc >= poor_threshold").index
+    item_summary_frame.loc[acceptable_pbc_indices, "is_acceptable_pbc"] = 1
+    acceptable_effective_distractors_indices = item_summary_frame.query("effective_distractors >= 1").index
+    item_summary_frame.loc[acceptable_effective_distractors_indices, "is_acceptable_effective_distractors"] = 1
+
+    item_summary_frame["estimated_item_difficulty_tier"] = item_summary_frame["is_good_estimated_item_difficulty"] + item_summary_frame["is_acceptable_estimated_item_difficulty"]
+    item_summary_frame["item_difficulty_tier"] = item_summary_frame["is_good_item_difficulty"] + item_summary_frame["is_acceptable_item_difficulty"]
+    item_summary_frame["pbc_tier"] = item_summary_frame["is_good_pbc"] + item_summary_frame["is_acceptable_pbc"]
+    item_summary_frame["effective_distractors_tier"] = item_summary_frame["is_good_effective_distractors"] + item_summary_frame["is_acceptable_effective_distractors"]
+    item_summary_frame["infit_tier"] = item_summary_frame["is_good_infit"] + item_summary_frame["is_acceptable_infit"]
+    item_summary_frame["outfit_tier"] = item_summary_frame["is_good_outfit"] + item_summary_frame["is_acceptable_outfit"]
     exam_ids = ["1A", "1B", "2A", "2B", "2C", "3A", "3B", "3C", "4A", "4B", "4C"]
     for exam_id in exam_ids:
         exam_item_summary_frame = item_summary_frame[item_summary_frame['question_id'].str.contains(exam_id)]
@@ -44,13 +65,18 @@ if __name__ == "__main__":
         
         
         columns_to_plot_data = ["var_estimates_items", "outfit_items", "infit_items", "effective_distractors", "pbc", "item_difficulty"]
-        columns_to_plot_colors = ["is_good_estimated_item_difficulty", "is_good_outfit", "is_good_infit", "is_good_effective_distractors", "is_good_pbc", "is_good_item_difficulty"]
+        columns_to_plot_colors = ["estimated_item_difficulty_tier", "outfit_tier", "infit_tier", "effective_distractors_tier", "pbc_tier", "item_difficulty_tier"]
         columns_to_display = ["Estimated Item Difficulty", "Outfit", "Infit", "Effective Distractors", "PBC", "Item Difficulty"]
 
         data = exam_item_summary_frame[columns_to_plot_data].to_numpy()
-        data_colors = exam_item_summary_frame[columns_to_plot_colors].to_numpy()
+        data_colors = exam_item_summary_frame[columns_to_plot_colors].to_numpy().astype(float)
+        
+        #This is to shift the colors in the heatmap a bit.
+        green_indices = np.greater(data_colors, 1.5)
+        data_colors[green_indices] += .35
+        
         fig, (ax) = plt.subplots(nrows=1, ncols=1, figsize=(10,6))
-        im, cbar = heatmap_utils.heatmap(data_colors, question_ids, columns_to_display, cmap = "RdYlGn", ax=ax, aspect="auto", vmin = -1.5, vmax = 1.5)
+        im, cbar = heatmap_utils.heatmap(data_colors, question_ids, columns_to_display, cmap = "RdYlGn", ax=ax, aspect="auto", vmin = -.65, vmax = 2.65)
         cbar.remove()
         for i in range(data.shape[0]):
             for j in range(data.shape[1]):
